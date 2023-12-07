68cc2c63e6a47416cb95557974a3b77e1afa6260
henry wan <wanhengiot@163.com>
2023-12-07 21:55:12 +0800

    完成游戏界面，实现蛇的移动

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { GameObject } from "./GameObject";
+import { Snake } from "./Snake";
 import { Wall } from "./Wall";
 
 export class GameMap extends GameObject {
@@ -11,21 +12,33 @@ export class GameMap extends GameObject {
         this.L = 0; // L表示一个单位的长度
 
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         // 随机障碍物数量，关于主对角线对称
-        this.inner_walls_count = 20;
+        this.inner_walls_count = 10;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
+        ]
+        // 左下角的蛇初始眼睛朝上
+        this.eye_direction = 0;
+        // 右上角的蛇初始眼睛朝下
+        if (this.id == 1) this.eye_direction = 2;
+
     }
 
     // 判断连通性
     check_connnectivity(g, sx, sy, tx, ty) {
-        if (sx == tx && sy == ty) return true;
+        if (sx === tx && sy === ty) return true;
         g[sx][sy] = true;
         let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
         for (let i = 0; i < 4; i++) {
-            let x = sx + dx[i], y = sy + dy[i];
-            if (!g[x][y] && this.check_connnectivity(g, x, y, tx, ty)) return true;
+            let x = sx + dx[i];
+            let y = sy + dy[i];
+            if (!g[x][y] && this.check_connnectivity(g, x, y, tx, ty))
+                return true;
         }
         return false;
     }
@@ -52,10 +65,10 @@ export class GameMap extends GameObject {
             for (let j = 0; j < 200; j++) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 // 防止两条蛇起点被障碍物覆盖
-                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) continue;
-                g[r][c] = g[c][r] = true;
+                if (r === this.rows - 2 && c === 1 || r === 1 && c === this.cols - 2) continue;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -74,11 +87,38 @@ export class GameMap extends GameObject {
         return true;
     }
 
+    add_listening_events() {
+        // 获取自动聚焦
+        this.ctx.canvas.focus();
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            console.log(e.key);
+            if (e.key === 'w') { snake0.set_direction(0); }
+            else if (e.key === 'd') { snake0.set_direction(1); }
+            else if (e.key === 's') { snake0.set_direction(2); }
+            else if (e.key === 'a') { snake0.set_direction(3); }
+            else if (e.key === 'ArrowUp') { snake1.set_direction(0); }
+            else if (e.key === 'ArrowRight') { snake1.set_direction(1); }
+            else if (e.key === 'ArrowDown') { snake1.set_direction(2); }
+            else if (e.key === 'ArrowLeft') { snake1.set_direction(3); }
+        });
+    }
+
     start() {
         for (let i = 0; i < 1000; i++) {
             // 保证两蛇之间是连通的
-            if (this.create_walls()) break;
+            if (this.create_walls())
+                break;
         }
+        this.add_listening_events();
+    }
+
+    check_ready() {
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
     }
 
     update_size() {
@@ -87,8 +127,46 @@ export class GameMap extends GameObject {
         this.ctx.canvas.width = this.L * this.cols;
         this.ctx.canvas.height = this.L * this.rows;
     }
+
+    // 让两条蛇进入下一回合
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
+    // 检测目标位置是否合法, 两蛇身体和墙没有相撞
+    check_vaild(cell) {
+        // 判断是否越界
+        if (cell.r < 0 || cell.r >= this.rows || cell.c < 0 || cell.c >= this.cols) {
+            return false;
+        }
+        // 判断是否与障碍物相撞
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) {
+                return false;
+            }
+        }
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            // 如果蛇尾会缩进，蛇尾不用判断
+            if (!snake.check_tail_increasing()) {
+                k--;
+            }
+            for (let i = 0; i < k; i++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+
     update() {
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
     // 渲染
@@ -105,6 +183,7 @@ export class GameMap extends GameObject {
                 this.ctx.fillRect(this.L * c, this.L * r, this.L, this.L);
             }
         }
+
     }
 
 }
---
+++ web/src/assets/scripts/Snake.js
@@ -0,0 +1,170 @@
+import { GameObject } from "./GameObject";
+import { Cell } from "./Cell";
+
+export class Snake extends GameObject {
+    constructor(info, gamemap) {
+        super();
+
+        this.id = info.id;
+        this.color = info.color;
+        this.gamemap = gamemap;
+
+        // 存放蛇的身体，cells[0]存放蛇头
+        this.cells = [new Cell(info.r, info.c)];
+        this.next_cell = null;
+
+
+        // 蛇每秒走5个格子
+        this.speed = 5;
+        // -1 表示没有指令，0 1 2 3 表示上右下左
+        this.direction = -1;
+        // idle 静止 move 移动 die 已经死亡
+        this.status = "idle";
+        this.dr = [-1, 0, 1, 0]; // 行偏移量
+        this.dc = [0, 1, 0, -1]; // 列偏移量
+
+        this.step = 0; // 表示当前回合数
+        this.eps = 1e-2; // 允许的误差
+
+        // 初始化蛇眼睛
+        // 左下角的蛇初始眼睛朝上
+        this.eye_direction = 0;
+        // 右上角的蛇初始眼睛朝下
+        if (this.id == 1) this.eye_direction = 2;
+
+        // 蛇眼睛x方向 ：不同方向的偏移量
+        this.eye_dx = [
+            [-1, 1],
+            [1, 1],
+            [-1, 1],
+            [-1, -1]
+        ];
+
+        // 蛇眼睛y方向： 不同方向的偏移量
+        this.eye_dy = [
+            [-1, -1],
+            [-1, 1],
+            [1, 1],
+            [-1, 1]
+        ];
+    }
+
+    start() {
+
+    }
+
+    // 检测当前回合，蛇的长度是否增加
+    check_tail_increasing() {
+        if (this.step <= 10) return true;
+        if (this.step % 3 === 0) return true;
+        return false;
+    }
+
+    set_direction(d) {
+        this.direction = d;
+    }
+
+    // 将蛇的状态变为走下一步
+    next_step() {
+        const d = this.direction; // 获取蛇的方向
+        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d]);
+        this.eye_direction = d;
+        this.direction = -1;
+        this.status = "move";
+        this.step++;
+
+        const k = this.cells.length;
+        // 头部不变
+        for (let i = k; i > 0; i--) {
+            // 防止引用间互相干扰，使用json转
+            this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
+        }
+        // 下一步操作蛇撞了，蛇应该瞬间去世
+        if (!this.gamemap.check_vaild(this.next_cell)) {
+            this.status = "die";
+        }
+    }
+
+    update_move() {
+        const dx = this.next_cell.x - this.cells[0].x;
+        const dy = this.next_cell.y - this.cells[0].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // 说明已经重合了
+        if (distance < this.eps) {
+            this.cells[0] = this.next_cell; // 添加一个新蛇头
+            this.next_cell = null;
+
+            this.status = "idle"; // 走完了， 停下了
+            // 蛇不变长
+            if (!this.check_tail_increasing()) {
+                this.cells.pop();
+            }
+
+        }
+        else {
+            const move_distance = this.speed * this.timedelta / 1000;
+            this.cells[0].x += move_distance * dx / distance;
+            this.cells[0].y += move_distance * dy / distance;
+
+            if (!this.check_tail_increasing()) {
+                const k = this.cells.length;
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2];
+                const tail_dx = tail_target.x - tail.x;
+                const tail_dy = tail_target.y - tail.y;
+                tail.x += move_distance * tail_dx / distance;
+                tail.y += move_distance * tail_dy / distance;
+            }
+        }
+    }
+
+    // 每一帧执行一次
+    update() {
+        if (this.status === "move") {
+            console.log("move");
+            this.update_move();
+        }
+        this.render();
+    }
+    render() {
+        const L = this.gamemap.L;
+        const ctx = this.gamemap.ctx;
+
+        // 后渲染的颜色会覆盖前面渲染的颜色
+        ctx.fillStyle = this.color;
+
+        if (this.status === "die") {
+            ctx.fillStyle = "white";
+        }
+
+        for (const cell of this.cells) {
+            ctx.beginPath();
+            ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, Math.PI * 2);
+            ctx.fill();
+        }
+
+        // 美化蛇的身体
+        for (let i = 1; i < this.cells.length; i++) {
+            const a = this.cells[i - 1], b = this.cells[i];
+            if (Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps)
+                continue;
+            // 竖直方向
+            if (Math.abs(a.x - b.x) < this.eps) {
+                ctx.fillRect((a.x - 0.4) * L, Math.min(a.y, b.y) * L, L * 0.8, Math.abs(a.y - b.y) * L);
+            }
+            // 水平方向
+            else {
+                ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L * 0.8);
+            }
+        }
+
+        ctx.fillStyle = "black";
+        for (let i = 0; i < 2; i++) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.15) * L;
+            ctx.beginPath();
+            ctx.arc(eye_x, eye_y, L * 0.05, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+}
