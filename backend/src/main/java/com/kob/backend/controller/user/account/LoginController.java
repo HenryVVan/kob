a3a9740106e25281fb7b87da06d8ea63dcac05a8
henry wan <wanhengiot@163.com>
2023-12-27 11:31:26 +0800

    完成退出，以及路由未授权验证

--- backend/src/main/java/com/kob/backend/controller/user/account/LoginController.java
+++ backend/src/main/java/com/kob/backend/controller/user/account/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
